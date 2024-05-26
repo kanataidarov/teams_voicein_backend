53766d3f40c22566c51b9316f3e7207c59936b86
Kanat Aidarov <kanataidarov@yahoo.com>
2024-05-26 14:27:26 +0500

    client received recognition result finally

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,27 +5,35 @@ import (
 	"context"
 	"fmt"
 	"github.com/kanataidarov/tinkoff_voicekit/internal/config"
+	"github.com/kanataidarov/tinkoff_voicekit/pkg/args"
+	"github.com/kanataidarov/tinkoff_voicekit/pkg/common"
 	pb "github.com/kanataidarov/tinkoff_voicekit/pkg/teams_voicein"
+	sttPb "github.com/kanataidarov/tinkoff_voicekit/pkg/tinkoff_voicekit/cloud/stt/v1"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"log/slog"
 	"net"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type server struct {
 	pb.UnimplementedSpeechToTextServer
 }
 
-func (s *server) Recognize(ctx context.Context, req *pb.SttRequest) (*pb.SttResponse, error) {
+func (s *server) Recognize(_ context.Context, req *pb.SttRequest) (*pb.SttResponse, error) {
 	var (
 		header *pb.FileHeader
 		buf    bytes.Buffer
 	)
 
 	header = req.Header
-	log.Printf("File name: %v", header.Name)
+	fileName := filepath.Base(header.Name)
+	log.Printf("File name: %v", fileName)
 	if header.Size != nil {
-		log.Printf("File size should be: %v", header.Size)
+		log.Printf("File size should be: %d", header.Size)
 	}
 	if data := req.Data; data != nil {
 		buf.Write(data)
@@ -33,7 +41,103 @@ func (s *server) Recognize(ctx context.Context, req *pb.SttRequest) (*pb.SttResp
 
 	log.Printf("Total bytes received: %v", buf.Len())
 
-	return &pb.SttResponse{Message: fmt.Sprintf("Received %v bytes. Thanks!", buf.Len())}, nil
+	res := doRecognize(buf, fileName)
+
+	return &pb.SttResponse{Message: res}, nil
+}
+
+func doRecognize(buf bytes.Buffer, fileName string) string {
+	prepareFile(buf, fileName)
+
+	opts := args.ParseRecognizeOptions()
+	if opts == nil {
+		os.Exit(1)
+	}
+	defer func(InputFile *os.File) {
+		_ = InputFile.Close()
+	}(opts.InputFile)
+
+	var dataReader io.Reader
+	if strings.HasSuffix(opts.InputFile.Name(), ".wav") {
+		reader, err := common.OpenWavFormat(opts.InputFile, "LINEAR16", 1, 16000)
+		if err != nil {
+			panic(err)
+		}
+		dataReader = reader
+	} else {
+		dataReader = opts.InputFile
+	}
+
+	client, err := common.NewSttClient(opts.CommonOptions)
+	if err != nil {
+		panic(err)
+	}
+	defer func(client common.SpeechToTextClient) {
+		_ = client.Close()
+	}(client)
+
+	contents, err := io.ReadAll(dataReader)
+	if err != nil {
+		panic(err)
+	}
+
+	request := &sttPb.RecognizeRequest{
+		Config: &sttPb.RecognitionConfig{
+			Encoding:                   sttPb.AudioEncoding(sttPb.AudioEncoding_value[*opts.Encoding]),
+			SampleRateHertz:            uint32(*opts.Rate),
+			LanguageCode:               *opts.LanguageCode,
+			MaxAlternatives:            uint32(*opts.MaxAlternatives),
+			ProfanityFilter:            !(*opts.DisableProfanityFilter),
+			EnableAutomaticPunctuation: !(*opts.DisableAutomaticPunctuation),
+			NumChannels:                uint32(*opts.NumChannels),
+		},
+		Audio: &sttPb.RecognitionAudio{
+			AudioSource: &sttPb.RecognitionAudio_Content{Content: contents},
+		},
+	}
+	if *opts.DoNotPerformVad {
+		request.Config.Vad = &sttPb.RecognitionConfig_DoNotPerformVad{DoNotPerformVad: true}
+	} else {
+		request.Config.Vad = &sttPb.RecognitionConfig_VadConfig{
+			VadConfig: &sttPb.VoiceActivityDetectionConfig{
+				SilenceDurationThreshold: float32(*opts.SilenceDurationThreshold),
+			},
+		}
+	}
+
+	// context.WithCancel in prod
+	result, err := client.Recognize(context.Background(), request)
+	if err != nil {
+		panic(err)
+	}
+
+	transcript := result.Results[0].Alternatives[0].Transcript
+
+	return transcript
+}
+
+func prepareFile(buf bytes.Buffer, fileName string) *os.File {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Print("Could not create file", err)
+		return nil
+	}
+
+	length, err := file.Write(buf.Bytes())
+	if err != nil {
+		_ = file.Close()
+		log.Print("Could not write to file", err)
+		return nil
+	}
+	log.Printf("Wrote %d bytes to file", length)
+
+	err = file.Close()
+	if err != nil {
+		log.Print("Could not close file", err)
+		return nil
+	}
+
+	return file
 }
 
 func Serve(cfg *config.Config, log *slog.Logger) {
